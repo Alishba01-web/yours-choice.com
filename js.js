83259abf9aae006a5356270e3551f4249456b94a
Alishba01-web <alishbaofficial657@gmail.com>
2024-10-04 09:16:47 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+var mainImage = document.getElementById("main");
+var smallImage = document.getElementsByClassName("smaller");
+
+ smallImage[0].onclick = function(){
+    mainImage.src = smallImage[0].src;
+
+}
+
+ smallImage[1].onclick = function(){
+    mainImage.src = smallImage[1].src;
+    
+}
+
+ smallImage[2].onclick = function(){
+    mainImage.src = smallImage[2].src;
+    
+}
+
+ smallImage[3].onclick = function(){
+    mainImage.src = smallImage[3].src;
+    
+}
